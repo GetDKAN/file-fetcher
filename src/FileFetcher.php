38a7f10c62217290250165c307aa492397c3f22d
fmizzell <fmizzell.dev@gmail.com>
2019-10-18 11:11:16 -0500

    Making FileFetcher a persistent job.

--- src/FileFetcher.php
+++ src/FileFetcher.php
@@ -6,7 +6,7 @@ use FileFetcher\Processor\Local;
 use FileFetcher\Processor\ProcessorInterface;
 use FileFetcher\Processor\Remote;
 use FileFetcher\Processor\LastResort;
-use Procrastinator\Job\Job;
+use Procrastinator\Job\AbstractPersistentJob;
 use Procrastinator\Result;
 
 /**
@@ -16,23 +16,32 @@ use Procrastinator\Result;
  * ### Basic Usage:
  * @snippet test/FileFetcherTest.php Basic Usage
  */
-class FileFetcher extends Job
+class FileFetcher extends AbstractPersistentJob
 {
     private $processors = [];
 
-    public function __construct($filePath, $temporaryDirectory = "/tmp")
+    /**
+     * Constructor.
+     */
+    protected function __construct(string $identifier, $storage, array $config = null)
     {
+        parent::__construct($identifier, $storage, $config);
+
+        if (!isset($config['temporaryDirectory'])) {
+            $config['temporaryDirectory'] = "/tmp";
+        }
+
         $this->processors = self::getProcessors();
 
         // [State]
 
         $state = [
-            'source' => $filePath,
+            'source' => $config['filePath'],
             'total_bytes' => 0,
             'total_bytes_copied' => 0,
             'temporary' => false,
-            'destination' => $filePath,
-            'temporary_directory' => $temporaryDirectory,
+            'destination' => $config['filePath'],
+            'temporary_directory' => $config['temporaryDirectory'],
         ];
 
         // [State]
@@ -77,11 +86,6 @@ class FileFetcher extends Job
         return $this->processors[$this->getStateProperty('processor')];
     }
 
-    protected function setState($state)
-    {
-        $this->getResult()->setData(json_encode($state));
-    }
-
     public function jsonSerialize()
     {
         $object = parent::jsonSerialize();
@@ -91,20 +95,24 @@ class FileFetcher extends Job
         return $object;
     }
 
-    public static function hydrate($json)
+    public static function hydrate(string $json, $instance = null)
     {
         $data = json_decode($json);
+        $object = $instance;
 
         $reflector = new \ReflectionClass(self::class);
-        $object = $reflector->newInstanceWithoutConstructor();
+
+        if (!$instance) {
+            $object = $reflector->newInstanceWithoutConstructor();
+        }
 
         $reflector = new \ReflectionClass($object);
 
-        $p = $reflector->getParentClass()->getProperty('timeLimit');
+        $p = $reflector->getParentClass()->getParentClass()->getProperty('timeLimit');
         $p->setAccessible(true);
         $p->setValue($object, $data->timeLimit);
 
-        $p = $reflector->getParentClass()->getProperty('result');
+        $p = $reflector->getParentClass()->getParentClass()->getProperty('result');
         $p->setAccessible(true);
         $p->setValue($object, Result::hydrate(json_encode($data->result)));
 
--- test/FileFetcherTest.php
+++ test/FileFetcherTest.php
@@ -2,20 +2,26 @@
 
 namespace FileFetcherTest;
 
+use Contracts\Mock\Storage\Memory;
 use FileFetcher\FileFetcher;
 use Procrastinator\Result;
 
 class FileFetcherTest extends \PHPUnit\Framework\TestCase
 {
-    private $sampleCsvSize = 50;
+    private $sampleCsvSize = 5;
 
     public function testRemote()
     {
         // [Basic Usage]
 
-        $fetcher = new \FileFetcher\FileFetcher(
-            "http://samplecsvs.s3.amazonaws.com/Sacramentorealestatetransactions.csv"
+        $fetcher = FileFetcher::get(
+            "1",
+            new Memory(),
+            [
+            "filePath" => "http://samplecsvs.s3.amazonaws.com/Sacramentorealestatetransactions.csv"
+            ]
         );
+
         $result = $fetcher->run();
 
         // [Basic Usage]
@@ -29,7 +35,15 @@ class FileFetcherTest extends \PHPUnit\Framework\TestCase
     public function testLocal()
     {
         $local_file = __DIR__ . "/files/tiny.csv";
-        $fetcher = new \FileFetcher\FileFetcher($local_file);
+
+        $fetcher = FileFetcher::get(
+            "1",
+            new Memory(),
+            [
+            "filePath" => $local_file
+            ]
+        );
+
         $fetcher->setTimeLimit(1);
         $result = $fetcher->run();
         $data = json_decode($result->getData());
@@ -39,10 +53,15 @@ class FileFetcherTest extends \PHPUnit\Framework\TestCase
 
     public function testTimeOut()
     {
-        $fetcher = new \FileFetcher\FileFetcher(
-            "https://dkan-default-content-files.s3.amazonaws.com/{$this->sampleCsvSize}_mb_sample.csv"
-        );
+        $store = new Memory();
+        $config = [
+          "filePath" => "https://dkan-default-content-files.s3.amazonaws.com/{$this->sampleCsvSize}_mb_sample.csv"
+        ];
+
+        $fetcher = FileFetcher::get("1", $store, $config);
+
         $file_size = $fetcher->getStateProperty('total_bytes');
+
         $this->assertLessThan($file_size, $fetcher->getStateProperty('total_bytes_copied'));
 
         $fetcher->setTimeLimit(1);
@@ -51,23 +70,30 @@ class FileFetcherTest extends \PHPUnit\Framework\TestCase
         $this->assertGreaterThan(0, $fetcher->getStateProperty('total_bytes_copied'));
         $this->assertEquals($fetcher->getResult()->getStatus(), \Procrastinator\Result::STOPPED);
 
-        $json = json_encode($fetcher);
-        $fetcher2 = \FileFetcher\FileFetcher::hydrate($json);
+        $fetcher2 = \FileFetcher\FileFetcher::get("1", $store, $config);
 
         $fetcher2->setTimeLimit(PHP_INT_MAX);
         $fetcher2->run();
         $this->assertEquals($file_size, $fetcher2->getStateProperty('total_bytes_copied'));
-        $this->assertEquals(
-            filesize("/tmp/dkan_default_content_files_s3_amazonaws_com_{$this->sampleCsvSize}_mb_sample.csv"),
-            $fetcher2->getStateProperty('total_bytes_copied')
-        );
+
+        clearstatcache();
+        $actualFileSize = filesize("/tmp/dkan_default_content_files_s3_amazonaws_com_{$this->sampleCsvSize}_mb_sample.csv");
+
+        $this->assertEquals($actualFileSize, $fetcher2->getStateProperty('total_bytes_copied'));
+
         $this->assertEquals($fetcher2->getResult()->getStatus(), \Procrastinator\Result::DONE);
     }
 
     public function testIncompatibleServer()
     {
         $url = "https://data.medicare.gov/api/views/42wc-33ci/rows.csv?accessType=DOWNLOAD&sorting=true";
-        $fetcher = new FileFetcher($url);
+        $fetcher = FileFetcher::get(
+            "1",
+            new Memory(),
+            [
+            "filePath" => $url
+            ]
+        );
         $fetcher->setTimeLimit(1);
         $result = $fetcher->run();
         $this->assertEquals(Result::DONE, $result->getStatus());
